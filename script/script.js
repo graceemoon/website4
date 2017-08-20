0fe7c17d08d76766b2b73138eaf3493e56145e6a
grace moon <hi.gracemoon@gmail.com>
2017-08-20 16:08:59 -0400

    sub menu for projects... grr

--- script/script.js
+++ script/script.js
@@ -1,11 +1,28 @@
+// $('.landing').click(function(){
+// 	// $(this).toggleClass('.remove-landing');
+// 	$('.landing').fadeOut('slow');
+// })
+
+
 $('.landing').click(function(){
-	// this.addClass('.remove-landing')
-	$('.landing').fadeOut('slow');
+  $('.landing').fadeOut(400, function() {
+    $(this).remove();
+  })
 })
 
 
-$('.about-button').click(function() {
-	$('html, body').animate({
-		scrollTop: $('.about').offset().top
-	}, 400);
+// $('.about-button').click(function() {
+// 	$('html, body').animate({
+// 		scrollTop: $('.about').offset().top
+// 	}, 400);
+// });
+
+
+$(function() {
+    $(".projects").toggle(function() {
+        $(this).next(".submenu").slideDown(200);
+    }, function() {
+        $(this).next(".submenu").slideUp(200);
+    });
 });
+
