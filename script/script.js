b0f91b8d557538412f47bac3b548dd476156156b
grace moon <hi.gracemoon@gmail.com>
2017-12-15 17:08:32 -0500

    new idea, make new repo

--- script/script.js
+++ script/script.js
@@ -4,38 +4,19 @@
 // })
 
 
-$('.landing').click(function(){
-  $('.landing').fadeOut(400, function() {
-    $(this).remove();
-  })
+// $('.landing').click(function(){
+//   $('.landing').fadeOut(400, function() {
+//     $(this).remove();
+//   })
+// })
+
+
+
+$(.about).click(function() {
+	let aboutPage = $(.about-page); 
+	aboutPage.animate( {
+		opacity: '0.4',
+		z-index: '99',
+	});
+	
 })
-
-
-// $('.about-button').click(function() {
-// 	$('html, body').animate({
-// 		scrollTop: $('.about').offset().top
-// 	}, 400);
-// });
-
-// $('.submenu').hide();
-// $('.parent').children().click(function(){
-//     event.preventDefault();
-//     $(this).children('.child').slideToggle(200);     
-// });
-
-// $(function() {
-    // $(".projects").toggle(function() {
-    //     $(this).next(".submenu").slideDown(200);
-    // }, function() {
-    //     $(this).next(".submenu").slideUp(200);
-    // });
-// });
-
-// $('.projects').toggle(durationFunction, completionFunction);
-// $('.projects').on('click', function() {
-// 	console.log('toggling');
-// });
-
-$('.projects').on('click', function() {
-	$('.submenu').toggle(150);
-});
